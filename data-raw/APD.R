59260738e652776a91d79395e4e5b8c1ef8067c3
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-08-02 08:39:14 +0100

    Add raw data script for the APD

---
+++ data-raw/APD.R
@@ -0,0 +1,19 @@
+## code to prepare `APD` dataset goes here
+`%>%` <- magrittr::`%>%`
+output <- smpds::process_apd("~/Downloads/SMPDSv2/APD/",
+                             col_names = c("Taxon Name [APD]",
+                                           "Taxon Name [Author]",
+                                           "Depth [m]",
+                                           "Radiocarbon Chronology [yrs BP]",
+                                           "Calendar Chronology [yrs BP]",
+                                           "Count"),
+                             col_types = c(readr::col_character(),
+                                           readr::col_character(),
+                                           readr::col_double(),
+                                           readr::col_double(),
+                                           readr::col_double(),
+                                           readr::col_double()))
+APD <- output %>%
+  purrr::map_df(~.x)
+
+usethis::use_data(APD, overwrite = TRUE)
