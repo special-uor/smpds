d103e6d5a073c6ef47fbe1b38ad6e7640742e347
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-08-05 15:55:07 +0100

    Minor changes

--- data-raw/EMPD.R
+++ data-raw/EMPD.R
@@ -1,4 +1,7 @@
 ## code to prepare `EMPD` dataset goes here
+# The Eurasian Modern Pollen Database
+# https://doi.pangaea.de/10.1594/PANGAEA.909130?format=html#download
+# https://essd.copernicus.org/articles/12/2423/2020/
 sheets <- c("metadata", "climate", "ecosystems", "counts", "p_vars", "sampleContexts", "sampleTypes", "sampleMethods", "workerRoles", "countries", "ageUncertainties", "locationReliabilities", "groupID")
 sheets <- c("metadata", "counts", "p_vars")
 `%>%` <- magrittr::`%>%`
@@ -11,6 +14,10 @@ empdv2_workbook <- sheets %>%
 
 empdv2_str <- empdv2_workbook %>%
   purrr::map(~names(.x))
+
+# ------------------------------------------------------------------------------
+# |                               Load metadata                                |
+# ------------------------------------------------------------------------------
 empdv2_metadata <- empdv2_workbook$metadata %>%
   dplyr::select(-dplyr::starts_with("Worker")) %>%
   dplyr::group_by(SampleName) %>%
@@ -40,31 +47,82 @@ empdv2_metadata <- empdv2_workbook$metadata %>%
                 vegetation_description = VegDescription,
                 entity_type = SampleType,
                 sample_method = SampleMethod,
-                age_BP = AgeBP)
+                age_BP = AgeBP) %>%
+  dplyr::mutate(ID_EMPDv2 = seq_along(entity_name), .before = 1)
 
 empdv2_metadata_workers <- empdv2_workbook$metadata %>%
   dplyr::select(dplyr::starts_with("Worker"))
 
+# Construct single table with metadata (based on SMPDSv1)
+EMPD <- empdv2_metadata %>%
+  dplyr::select(ID_EMPDv2,
+                source = EMPD_version,
+                site_name,
+                entity_name,
+                latitude,
+                longitude,
+                elevation,
+                basin_size = area_of_site,
+                site_type = site_description,
+                entity_type = entity_type,
+                age_BP,
+                publication = Publication,
+                DOI) %>%
+  dplyr::mutate(basin_size = basin_size * 0.01) # hectares to km2)
+
+# ------------------------------------------------------------------------------
+# |                             Extract count data                             |
+# ------------------------------------------------------------------------------
 empdv2_counts <- empdv2_workbook$counts %>%
   dplyr::rename(entity_name = SampleName,
-                taxon_name = acc_varname) %>%
+                taxon_name = original_varname
+                # taxon_name = acc_varname
+                ) %>%
+  dplyr::arrange(entity_name,
+                 taxon_name) %>%
   dplyr::mutate(ID_COUNT = seq_along(entity_name), .before = 1)
 
+## Search duplicated counts
+empdv2_counts_unique <- empdv2_counts %>%
+  dplyr::distinct(entity_name, taxon_name, .keep_all = TRUE)
+empdv2_counts_dup <- empdv2_counts %>%
+  dplyr::filter(!(ID_COUNT %in% empdv2_counts_unique$ID_COUNT))
+
+tmp <- empdv2_counts_dup %>%
+  # dplyr::slice(1:10) %>%
+  purrr::pmap_df(function(entity_name, taxon_name, ...) {
+    ent <- entity_name
+    tax <- taxon_name
+    empdv2_counts %>%
+      dplyr::filter(entity_name == ent,
+                    taxon_name == tax)
+  })
+
+# Create wide version of the unique counts
+empdv2_counts_unique_wide <- empdv2_counts_unique %>%
+  tidyr::pivot_wider(id_cols = entity_name, names_from = taxon_name, values_from = count) %>%
+  dplyr::select(1, order(colnames(.)[-1]) + 1) # Sort the taxon_names alphabetically
+
+# Attach counts to metadata
+EMPDv2 <- EMPD %>%
+  dplyr::full_join(empdv2_counts_unique_wide,
+                   by = "entity_name")
+
 # Find any matches in the SMPDSv1
-aux <- SMPDSv1_long %>%
-  dplyr::filter(entity_name %in% empdv2_counts$entity_name)
-aux <- empdv2_counts %>%
+# aux <- SMPDSv1_long %>%
+#   dplyr::filter(entity_name %in% empdv2_counts$entity_name)
+empdv2_counts_subset <- empdv2_counts %>%
   dplyr::filter(entity_name %in% SMPDSv1_long$entity_name,
                 taxon_name %in% SMPDSv1_long$taxon_name)
+# aux <- empdv2_counts %>%
+#   dplyr::filter(stringr::str_extract(entity_name, "[a-zA-Z]*") %in% SMPDSv1_long$short_entity_name,
+#                 taxon_name %in% SMPDSv1_long$taxon_name)
 empdv2_counts_filtered <- empdv2_counts %>%
-  dplyr::filter(!(ID_COUNT %in% aux$ID_COUNT))
+  dplyr::filter(!(ID_COUNT %in% empdv2_counts_subset$ID_COUNT))
 
-SMPDSv1_prefix <- SMPDSv1 %>%
-  dplyr::mutate(short_entity_name = entity_name %>%
-                  stringr::str_extract("[a-zA-Z]*"))
-# aux0 <- SMPDSv1_long %>%
-#   dplyr::filter(entity_name %in% aux$entity_name,
-#                 taxon_name %in% aux$taxon_name)
+empdv2_counts %>%
+  dplyr::distinct(entity_name, taxon_name, .keep_all = TRUE)
+  tidyr::pivot_wider(id_cols = ID_COUNT, names_from = taxon_name, values_from = count)
 
 
-# usethis::use_data(EMPD, overwrite = TRUE)
+usethis::use_data(EMPDv2, overwrite = TRUE)
