bf6656f59a361cbb5153de56700ccad7add5dfd0
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-08-12 17:52:22 +0100

    Update APD to include only modern records, -72 to 50

--- data-raw/APD.R
+++ data-raw/APD.R
@@ -18,25 +18,36 @@ apd_clean_taxon_names <- readr::read_csv("inst/extdata/apd_taxon.csv")
 
 APD <- output %>%
   purrr::map_df(~.x) %>%
+  dplyr::ungroup() %>%
   dplyr::rename(taxon_name = `Taxon Name [APD]`,
-                entity_name = name,
-                value = Count) %>%
+                taxon_name_author = `Taxon Name [Author]`,
+                site_name = sitename,
+                depth_in_m = `Depth [m]`,
+                age_BP = `Calendar Chronology [yrs BP]`,
+                count = Count) %>%
+  dplyr::select(-`Radiocarbon Chronology [yrs BP]`) %>%
+  dplyr::filter(is.na(age_BP) | age_BP <= 100) %>%
   dplyr::mutate(taxon_name = taxon_name %>%
                   stringr::str_squish()) %>%
   dplyr::left_join(apd_clean_taxon_names,
                    by = "taxon_name") %>%
-  dplyr::filter(action != "delete") %>%
-  # dplyr::select(-action) %>%
+  dplyr::filter(is.na(action) | action != "delete") %>%
+  dplyr::select(-action) %>%
   dplyr::rename(taxon_name_original = taxon_name,
-                taxon_name = clean_name) # %>%
-  # dplyr::group_by(entity_name, taxon_name) %>%
-  # dplyr::mutate(value = sum(as.double(value), na.rm = TRUE)) %>%
+                taxon_name = clean_name) %>%
+  dplyr::relocate(taxon_name, .before = taxon_name_original) %>%
+  dplyr::relocate(publication, .after = count) %>%
+  dplyr::mutate(taxon_name = ifelse(is.na(taxon_name),
+                                    "[ODD taxon]",
+                                    taxon_name)) %>%
+  # dplyr::group_by(site_name, taxon_name) %>%
+  # dplyr::mutate(count = sum(as.double(count), na.rm = TRUE)) %>%
   # dplyr::ungroup() %>%
   # dplyr::mutate(taxon_name = ifelse(is.na(taxon_name),
   #                                   taxon_name_original,
   #                                   taxon_name)) %>%
-  # dplyr::distinct(entity_name, taxon_name, .keep_all = TRUE) %>%
-  # dplyr::select(-taxon_name_original)
+  # dplyr::distinct(site_name, taxon_name, .keep_all = TRUE) %>%
+  dplyr::select(-taxon_name_original, -taxon_name_author)
 
 
 usethis::use_data(APD, overwrite = TRUE, compress = "xz")
@@ -67,7 +78,36 @@ apd_taxa_names <- readxl::read_xlsx("~/Downloads/SMPDSv2/smpdsv2-APD-Herzschuh-t
   dplyr::distinct(taxon_name, clean_name, .keep_all = TRUE)
 
 
+aux <- output %>%
+  purrr::map_df(~.x) %>%
+  dplyr::rename(taxon_name = `Taxon Name [APD]`,
+                taxon_name_author = `Taxon Name [Author]`,
+                site_name = name,
+                depth_in_m = `Depth [m]`,
+                age_BP = `Calendar Chronology [yrs BP]`,
+                count = Count) %>%
+  dplyr::select(-`Radiocarbon Chronology [yrs BP]`) %>%
+  dplyr::mutate(taxon_name = taxon_name %>%
+                  stringr::str_squish())
+apd_odd_taxon <- apd_clean_taxon_names %>%
+  dplyr::filter(is.na(action)) %>%
+  .$taxon_name
 aux2 <- aux %>%
-  dplyr::filter(taxon_name %in%
-                  apd_clean_taxon_names$taxon_name[is.na(apd_clean_taxon_names$action)]) %>%
+  dplyr::filter(taxon_name %in% apd_odd_taxon) %>%
+  dplyr::filter(is.na(age_BP) | age_BP <= 100) %>%
   dplyr::arrange(taxon_name)
+
+APD %>%
+  dplyr::filter(is.na(age_BP) | age_BP <= 50) %>%
+  dplyr::arrange(site_name, depth_in_m) %>%
+  readr::write_excel_csv("~/Downloads/SMPDSv2/APD-modern-records_all.csv", na = "")
+
+APD %>%
+  dplyr::filter(!is.na(age_BP) & (age_BP >= -72 & age_BP <= 50)) %>%
+  dplyr::arrange(site_name, depth_in_m) %>%
+  readr::write_excel_csv("~/Downloads/SMPDSv2/APD-modern-records_50_-72.csv", na = "")
+
+APD %>%
+  dplyr::filter(!is.na(age_BP) & (age_BP < -72)) %>%
+  dplyr::arrange(site_name, depth_in_m) %>%
+  readr::write_excel_csv("~/Downloads/SMPDSv2/APD-modern-records_over_-72.csv")
