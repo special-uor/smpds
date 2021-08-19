d265e47f7768f0112c84c606287e39c2c5045d9f
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-08-19 12:43:49 +0100

    Re-structure the PNV_classes data frame

--- data-raw/PNV.R
+++ data-raw/PNV.R
@@ -4,7 +4,9 @@
 URL <- "https://dataverse.harvard.edu/dataset.xhtml;jsessionid=9ffbf112e8caa68abc4dcfc0d1a9?persistentId=doi%3A10.7910%2FDVN%2FQQHCIK&version=&q=&fileTypeGroupFacet=&fileAccess=&fileSortField=size#"
 PNV <- raster::brick("./inst/extdata/pnv_biome.type_biome00k_c_1km_s0..0cm_2000..2017_v0.1.tif")
 PNV_classes <- readxl::read_xlsx("./inst/extdata/pnv_basic_info.xlsx") %>%
-  magrittr::set_names(c("ID_BIOME", "colour", "description"))
+  magrittr::set_names(c("ID_BIOME", "colour", "description")) %>%
+  dplyr::select(1, 3, 2) %>%
+  dplyr::mutate(colour = stringr::str_to_upper(colour))
 # Amalgamate the tundras
 PNV[PNV == 30] <- 28
 PNV[PNV == 31] <- 28
