9188636a52f50980f39102ed98236749db23d010
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-08-26 14:51:37 +0100

    Update code to read manually inspected files

--- data-raw/APD.R
+++ data-raw/APD.R
@@ -1,5 +1,67 @@
 ## code to prepare `APD` dataset goes here
 `%>%` <- magrittr::`%>%`
+
+apd_clean_taxon_names <- readr::read_csv("inst/extdata/apd_taxa.csv")
+
+APD_SPH <- readxl::read_xlsx("~/Downloads/SMPDSv2/APD-modern-records_diagnosed/APD-modern-records_all_SPH_diagnosed.xlsx",
+                             sheet = 1) %>%
+  dplyr::rename(SPH_comment = ...14) %>%
+  dplyr::mutate(ID_APD = seq_along(sigle))
+APD_SPH2 <- APD_SPH %>%
+  dplyr::filter(!is.na(age_BP))
+APD_SPH_unused2 <- APD_SPH %>%
+  dplyr::filter(!(ID_APD %in% APD_SPH2$ID_APD)) %>%
+  dplyr::arrange(sigle)
+
+
+APD_all <- APD_SPH2 %>%
+  dplyr::mutate(taxon_name = taxon_name %>%
+                  stringr::str_squish()) %>%
+  dplyr::filter(taxon_name != "[ODD taxon]") %>%
+  dplyr::select(-taxon_name_original,-taxon_name_author, -SPH_comment, -ID_APD) %>%
+  dplyr::group_by(site_name) %>% # Add suffix based on site_name and depth
+  # dplyr::mutate(entity_name = paste0(sigle, "_", depth_in_m, "_", age_BP))
+  dplyr::mutate(n = length(unique(depth_in_m)),
+                entity_name = ifelse(n > 1,
+                                     paste0(sigle, "_", depth_in_m), sigle),
+                .after = site_name) %>%
+  dplyr::group_by(entity_name) %>% # Add suffix based on entity_name and age_BP
+  dplyr::mutate(n = length(unique(age_BP)),
+                entity_name = ifelse(n > 1,
+                                     paste0(entity_name,
+                                            c("", paste0("_", seq_len(n)[-1]))),
+                                     entity_name),
+                .after = site_name) %>%
+  dplyr::group_by(entity_name, taxon_name) %>%
+  dplyr::mutate(count = sum(count, na.rm = TRUE),
+                n = length(count)) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct(entity_name, taxon_name, .keep_all = TRUE) %>%
+  dplyr::select(site_name, entity_name, latitude, longitude, elevation, age_BP, publication, taxon_name, count) %>%
+  dplyr::mutate(ID_APD = seq_along(site_name), .before = 1)
+
+APD_all_wide <- APD_all %>%
+  dplyr::ungroup() %>%
+  tidyr::pivot_wider(2:10, names_from = "taxon_name", values_from = "count") %>%
+  smpds::parallel_extract_biome(buffer = 12000, cpus = 6) %>%
+  dplyr::relocate(ID_BIOME, .after = age_BP)
+  # dplyr::mutate(ID_BIOME = tibble::tibble(latitude, longitude) %>%
+  #                 smpds::parallel_extract_biome(buffer = 12000, cpus = 6) %>%
+  #                 .$ID_BIOME,
+  #               .after = age_BP)
+APD_all_wide %>%
+  dplyr::filter(is.na(ID_BIOME))
+
+APD <- APD_all_wide %>%
+  dplyr::mutate(ID_BIOME = ifelse(is.na(ID_BIOME),
+                                  -888888,
+                                  ID_BIOME))
+
+usethis::use_data(APD, overwrite = TRUE, compress = "xz")
+
+# ------------------------------------------------------------------------------
+# |                             Raw data (.ASCII)                              |
+# ------------------------------------------------------------------------------
 output <- smpds::process_apd("~/Downloads/SMPDSv2/APD/",
                              col_names = c("Taxon Name [APD]",
                                            "Taxon Name [Author]",
@@ -13,23 +75,6 @@ output <- smpds::process_apd("~/Downloads/SMPDSv2/APD/",
                                            readr::col_double(),
                                            readr::col_double(),
                                            readr::col_double()))
-
-apd_clean_taxon_names <- readr::read_csv("inst/extdata/apd_taxa.csv")
-
-APD_SPH <- readxl::read_xlsx("~/Downloads/SMPDSv2/APD-modern-records_diagnosed/APD-modern-records_all_SPH_diagnosed.xlsx",
-                             sheet = 1) %>%
-  dplyr::rename(SPH_comment = ...14) %>%
-  dplyr::mutate(ID_APD = seq_along(sigle))
-APD_SPH2 <- APD_SPH %>%
-  dplyr::filter(
-    !is.na(age_BP)
-    # age_BP %>% stringr::str_detect("modern") |
-    #   SPH_comment %>% stringr::str_detect("^modern|^this might") |
-    #   is.na(SPH_comment)
-  )
-APD_SPH_unused2 <- APD_SPH %>%
-  dplyr::filter(!(ID_APD %in% APD_SPH2$ID_APD)) %>%
-  dplyr::arrange(sigle)
 
 APD <- output %>%
   purrr::map_df(~.x) %>%
@@ -63,37 +108,6 @@ APD <- output %>%
   #                                   taxon_name)) %>%
   # dplyr::distinct(site_name, taxon_name, .keep_all = TRUE) %>%
   dplyr::select(-taxon_name_original, -taxon_name_author)
-
-APD_all <- APD_SPH2 %>%
-  dplyr::mutate(taxon_name = taxon_name %>%
-                  stringr::str_squish()) %>%
-  dplyr::filter(taxon_name != "[ODD taxon]") %>%
-  dplyr::select(-taxon_name_original,-taxon_name_author, -SPH_comment, -ID_APD) %>%
-  dplyr::mutate(entity_name = paste0(site_name, "_", depth_in_m),
-                .after = site_name) %>%
-  dplyr::group_by(entity_name, taxon_name) %>%
-  dplyr::mutate(count = sum(count, na.rm = TRUE),
-                n = length(count)) %>%
-  dplyr::ungroup() %>%
-  dplyr::distinct(entity_name, taxon_name, .keep_all = TRUE) %>%
-  dplyr::select(site_name, entity_name, latitude, longitude, elevation, age_BP, publication, taxon_name, count) %>%
-  dplyr::mutate(ID_APD = seq_along(site_name), .before = 1)
-APD_all_wide <- APD_all %>%
-  dplyr::ungroup() %>%
-  tidyr::pivot_wider(2:10, names_from = "taxon_name", values_from = "count") %>%
-  dplyr::mutate(ID_BIOME = tibble::tibble(latitude, longitude) %>%
-                  smpds::parallel_extract_biome(buffer = 12000, cpus = 6) %>%
-                  .$ID_BIOME,
-                .after = age_BP)
-APD_all_wide %>%
-  dplyr::filter(is.na(ID_BIOME))
-
-APD <- APD_all_wide %>%
-  dplyr::mutate(ID_BIOME = ifelse(is.na(ID_BIOME),
-                                  -888888,
-                                  ID_BIOME))
-
-usethis::use_data(APD, overwrite = TRUE, compress = "xz")
 
 # ------------------------------------------------------------------------------
 # |                                APD Paradox                                 |
