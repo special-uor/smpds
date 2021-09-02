66111aec5a8132331513bcacfb295a90a8d5ca50
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-09-02 15:19:44 +0100

    Update test for the utils-biome module

--- tests/testthat/test-parallel_extract_biome.R
+++ tests/testthat/test-parallel_extract_biome.R
@@ -7,7 +7,7 @@ test_that("parallel_extract_biome works", {
     ~entity_name, ~latitude, ~longitude, ~ID_BIOME,
     "University of Reading",   51.4414,    -0.9418,        13
   )
-  expect_equal(parallel_extract_biome(test_data_t1), expected_t1)
+  expect_equal(parallel_extract_biome(test_data_t1, cpus = 1), expected_t1)
 
   # Data frame (tibble) as input with marine location (no ID_BIOME expected)
   test_data_t2 <- tibble::tibble(entity_name = "North Sea",
@@ -17,16 +17,18 @@ test_that("parallel_extract_biome works", {
     ~entity_name, ~latitude, ~longitude, ~ID_BIOME,
     "North Sea", 56.525027,   3.306268,        NA
   )
-  expect_equal(parallel_extract_biome(test_data_t2), expected_t2)
+  expect_equal(parallel_extract_biome(test_data_t2, cpus = 1), expected_t2)
 
   # Invalid data frame as input, missing longitude
   test_data_t3 <- tibble::tibble(entity_name = "University of Reading",
                                  latitude = 51.44140,
                                  longitude = NA)
-  expect_equal(parallel_extract_biome(test_data_t3), test_data_t3)
+  expect_equal(parallel_extract_biome(test_data_t3, cpus = 1), test_data_t3)
 
   # Invalid input format, list
-  expect_error(parallel_extract_biome(list(entity_name = "University of Reading",
-                                  latitude = 51.44140,
-                                  longitude = -0.9418)))
+  expect_error(parallel_extract_biome(
+    list(entity_name = "University of Reading",
+         latitude = 51.44140,
+         ongitude = -0.9418)
+    ))
 })
