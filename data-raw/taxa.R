7fcbdc79154cbc28b9242e552ecb3bea6c01cfb9
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-09-09 11:13:09 +0100

    Minor changes

--- data-raw/taxa.R
+++ data-raw/taxa.R
@@ -120,17 +120,18 @@ discrepancies <- taxa_clean_all %>%
   dplyr::filter(taxon_name %in% taxa_clean_all4$taxon_name) %>%
   dplyr::distinct(taxon_name, clean_name, .keep_all = TRUE) %>%
   dplyr::arrange(taxon_name, clean_name)
-discrepancies %>%
-  dplyr::select(-ID) %>%
-  dplyr::mutate(action = ifelse(!is.na(clean_name) & taxon_name == clean_name,
-                                "keep original name", action)) %>%
-  readr::write_excel_csv("~/Downloads/SMPDSv2/SMPDSv2_taxa_discrepancies_2021-09-02.csv", na = "EXCLUDE")
+# discrepancies %>%
+#   dplyr::select(-ID) %>%
+#   dplyr::mutate(action = ifelse(!is.na(clean_name) & taxon_name == clean_name,
+#                                 "keep original name", action)) %>%
+#   readr::write_excel_csv("~/Downloads/SMPDSv2/SMPDSv2_taxa_discrepancies_2021-09-02.csv", na = "EXCLUDE")
 
     # taxa_clean %>%
 #   dplyr::distinct(taxon_name, clean_name, .keep_all = TRUE)
 #   readr::write_excel_csv("inst/extdata/taxa_clean.csv", na = "")
 taxa_clean <- taxa_clean_all2 %>%
-  dplyr::select(-source, -ID)
+  dplyr::select(-source, -ID) %>%
+  dplyr::arrange(dplyr::desc(action), taxon_name)
 usethis::use_data(taxa_clean, taxa_amalgamation,
                   internal = TRUE,
                   overwrite = TRUE)
