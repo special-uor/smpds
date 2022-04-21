7e9e737ad860b032945de19346359e983676b300
Roberto Villegas-Diaz <bthillo@hotmail.com>
2022-04-21 15:54:58 +0100

    Update code to create a revised version of the APD

--- data-raw/APD.R
+++ data-raw/APD.R
@@ -1,4 +1,5 @@
 ## code to prepare `APD` dataset goes here
+## Original ----
 `%>%` <- magrittr::`%>%`
 
 apd_clean_taxon_names <- readr::read_csv("inst/extdata/apd_taxa.csv")
@@ -185,3 +186,221 @@ APD %>%
   dplyr::filter(!is.na(age_BP) & (age_BP < -72)) %>%
   dplyr::arrange(site_name, depth_in_m) %>%
   readr::write_excel_csv("~/Downloads/SMPDSv2/APD-modern-records_over_-72.csv")
+
+# SPH revisions ----
+"The entities and their counts were manually inspected by SPH"
+
+`%>%` <- magrittr::`%>%`
+## Load data ----
+APD_all <-
+  "data-raw/GLOBAL/APD_clean_SPH.xlsx" %>%
+  readxl::read_excel(sheet = 1) %>%
+  dplyr::mutate(ID_SAMPLE = seq_along(entity_name), .after = doi)
+
+### Metadata ----
+APD_metadata <-
+  APD_all %>%
+  dplyr::select(site_name:ID_SAMPLE) %>%
+  dplyr::rename(basin_size = `basin_size (km2)`)
+
+### Polen counts ----
+APD_counts <-
+  APD_all %>%
+  dplyr::select(ID_SAMPLE) %>%
+  dplyr::bind_cols(
+   APD_all %>% # Convert columns with counts to numeric type
+     dplyr::select(-c(site_name:ID_SAMPLE)) %>%
+     purrr::map_dfc(~.x %>% as.numeric)
+  ) %>%
+  magrittr::set_names(
+    colnames(.) %>%
+      stringr::str_replace_all("\\.\\.\\.", "#")
+  ) %>%
+  tidyr::pivot_longer(-ID_SAMPLE,
+                      names_to = "clean",
+                      values_to = "taxon_count") %>%
+  dplyr::mutate(clean = clean %>%
+                  stringr::str_remove_all("\\#[0-9]+$") %>%
+                  stringr::str_squish()) %>%
+  dplyr::group_by(ID_SAMPLE, clean) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::distinct() %>%
+  dplyr::ungroup()
+
+
+### Amalgamations ----
+APD_taxa_amalgamation <-
+  "data-raw/GLOBAL/APD_clean_SPH.xlsx" %>%
+  readxl::read_excel(sheet = 2) %>%
+  magrittr::set_names(c(
+    "clean", "intermediate", "amalgamated"
+  )) %>%
+  dplyr::distinct() %>%
+  dplyr::mutate(clean = clean %>% stringr::str_squish(),
+                intermediate = intermediate %>% stringr::str_squish(),
+                amalgamated = amalgamated %>% stringr::str_squish())
+
+### Combine counts and amalgamation ----
+APD_taxa_counts_amalgamation <-
+  APD_counts %>%
+  dplyr::left_join(APD_taxa_amalgamation,
+                   by = c("clean")) %>%
+  dplyr::relocate(taxon_count, .after = amalgamated) %>%
+  # dplyr::left_join(APD_metadata %>%
+  #                    dplyr::select(entity_name, ID_SAMPLE),
+  #                  by = "entity_name") %>%
+  # dplyr::select(-entity_name, -taxon_name) %>%
+  dplyr::relocate(ID_SAMPLE, .before = 1)
+
+APD_taxa_counts_amalgamation %>%
+  dplyr::filter(is.na(clean) | is.na(intermediate) | is.na(amalgamated))
+
+## Find DOIs ----
+APD_metadata_pubs <-
+  APD_metadata %>%
+  dplyr::distinct(publication) %>%
+  dplyr::arrange(publication) %>%
+  dplyr::mutate(DOI = publication %>%
+                  stringr::str_extract_all("\\[DOI\\s*(.*?)\\s*\\](;|$)") %>%
+                  purrr::map_chr(~.x %>%
+                                   stringr::str_remove_all("^\\[DOI:|\\]$") %>%
+                                   stringr::str_squish() %>%
+                                   stringr::str_c(collapse = ";\n"))
+  ) %>%
+  dplyr::mutate(ID_PUB = seq_along(publication))
+# APD_metadata_pubs %>%
+#   readr::write_excel_csv("data-raw/GLOBAL/APD_modern-references.csv")
+
+### Load cleaned publications list ----
+APD_clean_publications <-
+  "data-raw/GLOBAL/APD_modern-references_clean.csv" %>%
+  readr::read_csv() %>%
+  dplyr::select(-DOI)
+
+## Append clean publications ----
+APD_metadata_2 <-
+  APD_metadata %>%
+  dplyr::left_join(APD_metadata_pubs %>%
+                     dplyr::select(-DOI),
+                   by = "publication") %>%
+  dplyr::left_join(APD_clean_publications,
+                   by = "ID_PUB") %>%
+  dplyr::select(-publication.x, -publication.y, -doi, -ID_PUB) %>%
+  dplyr::rename(doi = updated_DOI,
+                publication = updated_publication)
+
+## Extract PNV/BIOME ----
+APD_metadata_3 <-
+  APD_metadata_2 %>%
+  dplyr::select(-dplyr::starts_with("ID_BIOME")) %>%
+  smpds::parallel_extract_biome(cpus = 12) %>%
+  # smpds::biome_name() %>%
+  dplyr::relocate(ID_BIOME, .after = doi) %>%
+  smpds::pb()
+
+APD_metadata_2 %>%
+  smpds::plot_biome()
+
+## Create count tables ----
+### Clean ----
+APD_clean <-
+  APD_taxa_counts_amalgamation %>%
+  dplyr::select(-intermediate, -amalgamated) %>%
+  dplyr::rename(taxon_name = clean) %>%
+  dplyr::group_by(ID_SAMPLE, taxon_name) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct() %>%
+  tidyr::pivot_wider(ID_SAMPLE,
+                     names_from = taxon_name,
+                     values_from = taxon_count,
+                     names_sort = TRUE)
+### Intermediate ----
+APD_intermediate <-
+  APD_taxa_counts_amalgamation %>%
+  dplyr::select(-clean, -amalgamated) %>%
+  dplyr::rename(taxon_name = intermediate) %>%
+  dplyr::group_by(ID_SAMPLE, taxon_name) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct() %>%
+  tidyr::pivot_wider(ID_SAMPLE,
+                     names_from = taxon_name,
+                     values_from = taxon_count,
+                     names_sort = TRUE)
+
+### Amalgamated ----
+APD_amalgamated <-
+  APD_taxa_counts_amalgamation %>%
+  dplyr::select(-clean, -intermediate) %>%
+  dplyr::rename(taxon_name = amalgamated) %>%
+  dplyr::group_by(ID_SAMPLE, taxon_name) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct() %>%
+  tidyr::pivot_wider(ID_SAMPLE,
+                     names_from = taxon_name,
+                     values_from = taxon_count,
+                     names_sort = TRUE)
+
+# Store subsets ----
+APD <-
+  APD_metadata_3 %>%
+  dplyr::mutate(
+    clean = APD_clean %>%
+      dplyr::select(-c(ID_SAMPLE)),
+    intermediate = APD_intermediate %>%
+      dplyr::select(-c(ID_SAMPLE)),
+    amalgamated = APD_amalgamated %>%
+      dplyr::select(-c(ID_SAMPLE))
+  ) %>%
+  dplyr::mutate(
+    basin_size = basin_size %>%
+      stringr::str_replace_all("unknown", "not known"),
+    entity_type = entity_type %>%
+      stringr::str_replace_all("unknown", "not known"),
+    site_type = site_type %>%
+      stringr::str_replace_all("unknown", "not known")
+  ) %>%
+  dplyr::relocate(ID_SAMPLE, .before = clean)
+
+usethis::use_data(APD, overwrite = TRUE, compress = "xz")
+
+## Inspect enumerates ----
+### basin_size -----
+APD$basin_size %>%
+  unique() %>% sort()
+
+### site_type ----
+APD$site_type %>%
+  unique() %>% sort()
+
+### entity_type ----
+APD$entity_type %>%
+  unique() %>% sort()
+
+# Export Excel workbook ----
+wb <- openxlsx::createWorkbook()
+openxlsx::addWorksheet(wb, "metadata")
+openxlsx::writeData(wb, "metadata",
+                    APD %>%
+                      dplyr::select(site_name:ID_SAMPLE))
+openxlsx::addWorksheet(wb, "clean")
+openxlsx::writeData(wb, "clean",
+                    APD %>%
+                      dplyr::select(ID_SAMPLE, clean) %>%
+                      tidyr::unnest(clean))
+openxlsx::addWorksheet(wb, "intermediate")
+openxlsx::writeData(wb, "intermediate",
+                    APD %>%
+                      dplyr::select(ID_SAMPLE, intermediate) %>%
+                      tidyr::unnest(intermediate))
+openxlsx::addWorksheet(wb, "amalgamated")
+openxlsx::writeData(wb, "amalgamated",
+                    APD %>%
+                      dplyr::select(ID_SAMPLE, amalgamated) %>%
+                      tidyr::unnest(amalgamated))
+openxlsx::saveWorkbook(wb,
+                       paste0("data-raw/GLOBAL/APD_",
+                              Sys.Date(),
+                              ".xlsx"))
