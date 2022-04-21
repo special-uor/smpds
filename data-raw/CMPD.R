142158ae20ad931c472b31a0d516a9b086a79a10
Roberto Villegas-Diaz <bthillo@hotmail.com>
2022-04-21 17:56:10 +0100

    Update code to include revised version of the CMPD

--- data-raw/CMPD.R
+++ data-raw/CMPD.R
@@ -1,4 +1,5 @@
 ## code to prepare `CMPD` dataset goes here
+# Original ----
 # The Chinese Modern Pollen Data by Ni Jian
 cmpd_counts <- readxl::read_xlsx("inst/extdata/cmpd_counts.xlsx",
                                  sheet = 2,
@@ -766,3 +767,229 @@ cmpd_counts2 <- cmpd_counts %>%
                           dplyr::left_join(cmpd_clean_taxon_names,
                                            by = "taxon_name") %>%
                           .$clean_name))
+
+# SPH revisions ----
+"The entities and their counts were manually inspected by SPH"
+
+`%>%` <- magrittr::`%>%`
+## Load data ----
+CMPD_all <-
+  "data-raw/GLOBAL/CMPD_clean_SPH.xlsx" %>%
+  readxl::read_excel(sheet = 1) %>%
+  dplyr::rename(doi = DOI) %>%
+  dplyr::mutate(ID_SAMPLE = seq_along(entity_name), .after = doi)
+
+### Metadata ----
+CMPD_metadata <-
+  CMPD_all %>%
+  dplyr::select(site_name:ID_SAMPLE)
+
+### Polen counts ----
+CMPD_counts <-
+  CMPD_all %>%
+  dplyr::select(ID_SAMPLE) %>%
+  dplyr::bind_cols(
+    CMPD_all %>% # Convert columns with counts to numeric type
+      dplyr::select(-c(source:ID_SAMPLE)) %>%
+      purrr::map_dfc(~.x %>% as.numeric)
+  ) %>%
+  magrittr::set_names(
+    colnames(.) %>%
+      stringr::str_replace_all("\\.\\.\\.", "#")
+  ) %>%
+  tidyr::pivot_longer(-ID_SAMPLE,
+                      names_to = "clean",
+                      values_to = "taxon_count") %>%
+  dplyr::mutate(clean = clean %>%
+                  stringr::str_remove_all("\\#[0-9]+$") %>%
+                  stringr::str_squish()) %>%
+  dplyr::group_by(ID_SAMPLE, clean) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::distinct() %>%
+  dplyr::ungroup()
+
+
+### Amalgamations ----
+CMPD_taxa_amalgamation <-
+  "data-raw/GLOBAL/CMPD_clean_SPH.xlsx" %>%
+  readxl::read_excel(sheet = 2) %>%
+  magrittr::set_names(c(
+    "clean", "intermediate", "amalgamated"
+  )) %>%
+  dplyr::distinct() %>%
+  dplyr::mutate(clean = clean %>% stringr::str_squish(),
+                intermediate = intermediate %>% stringr::str_squish(),
+                amalgamated = amalgamated %>% stringr::str_squish())
+
+### Combine counts and amalgamation ----
+CMPD_taxa_counts_amalgamation <-
+  CMPD_counts %>%
+  dplyr::left_join(CMPD_taxa_amalgamation,
+                   by = c("clean")) %>%
+  dplyr::relocate(taxon_count, .after = amalgamated) %>%
+  dplyr::relocate(ID_SAMPLE, .before = 1)
+
+CMPD_taxa_counts_amalgamation %>%
+  dplyr::filter(is.na(clean) | is.na(intermediate) | is.na(amalgamated)) %>%
+  dplyr::distinct(clean, intermediate, amalgamated)
+
+## Find DOIs ----
+CMPD_metadata_pubs <-
+  CMPD_metadata %>%
+  dplyr::distinct(publication, doi) %>%
+  dplyr::arrange(publication) %>%
+  dplyr::mutate(DOI = publication %>%
+                  stringr::str_extract_all("\\[DOI\\s*(.*?)\\s*\\](;|$)") %>%
+                  purrr::map_chr(~.x %>%
+                                   stringr::str_remove_all("^\\[DOI:") %>%
+                                   stringr::str_remove_all("\\]\\s*;\\s*$") %>%
+                                   stringr::str_remove_all("\\]$") %>%
+                                   stringr::str_remove_all("doi:") %>%
+                                   stringr::str_squish() %>%
+                                   stringr::str_c(collapse = ";\n")) #%>%
+                # stringr::str_replace_all("$\\s*\n0.1139/e80-122",
+                #                          "10.1139/e80-122")
+  ) %>%
+  dplyr::mutate(ID_PUB = seq_along(publication)) %>%
+  dplyr::mutate(updated_publication = NA, .before = publication) %>%
+  dplyr::mutate(updated_DOI = NA, .before = DOI)
+# CMPD_metadata_pubs %>%
+#   readr::write_excel_csv("data-raw/GLOBAL/CMPD_modern-references.csv")
+
+### Load cleaned publications list ----
+CMPD_clean_publications <-
+  "data-raw/GLOBAL/CMPD_modern-references_clean.csv" %>%
+  readr::read_csv() %>%
+  dplyr::select(-DOI)
+
+## Append clean publications ----
+CMPD_metadata_2 <-
+  CMPD_metadata %>%
+  dplyr::left_join(CMPD_metadata_pubs %>%
+                     dplyr::select(-DOI, -doi, -dplyr::contains("updated")),
+                   by = "publication") %>%
+  dplyr::left_join(CMPD_clean_publications,
+                   by = "ID_PUB") %>%
+  dplyr::select(-publication.x, -publication.y, -doi, -ID_PUB) %>%
+  dplyr::rename(doi = updated_DOI,
+                publication = updated_publication)
+
+## Extract PNV/BIOME ----
+CMPD_metadata_3 <-
+  CMPD_metadata_2 #%>%
+  # dplyr::select(-dplyr::starts_with("ID_BIOME")) %>%
+  # smpds::parallel_extract_biome(cpus = 12) %>%
+  # # smpds::biome_name() %>%
+  # dplyr::relocate(ID_BIOME, .after = doi) %>%
+  # smpds::pb()
+
+CMPD_metadata_3 %>%
+  smpds::plot_biome(xlim = range(.$longitude, na.rm = TRUE),
+                    ylim = range(.$latitude, na.rm = TRUE))
+
+## Create count tables ----
+### Clean ----
+CMPD_clean <-
+  CMPD_taxa_counts_amalgamation %>%
+  dplyr::select(-intermediate, -amalgamated) %>%
+  dplyr::rename(taxon_name = clean) %>%
+  dplyr::group_by(ID_SAMPLE, taxon_name) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct() %>%
+  tidyr::pivot_wider(ID_SAMPLE,
+                     names_from = taxon_name,
+                     values_from = taxon_count,
+                     names_sort = TRUE)
+### Intermediate ----
+CMPD_intermediate <-
+  CMPD_taxa_counts_amalgamation %>%
+  dplyr::select(-clean, -amalgamated) %>%
+  dplyr::rename(taxon_name = intermediate) %>%
+  dplyr::group_by(ID_SAMPLE, taxon_name) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct() %>%
+  tidyr::pivot_wider(ID_SAMPLE,
+                     names_from = taxon_name,
+                     values_from = taxon_count,
+                     names_sort = TRUE)
+
+### Amalgamated ----
+CMPD_amalgamated <-
+  CMPD_taxa_counts_amalgamation %>%
+  dplyr::select(-clean, -intermediate) %>%
+  dplyr::rename(taxon_name = amalgamated) %>%
+  dplyr::group_by(ID_SAMPLE, taxon_name) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct() %>%
+  tidyr::pivot_wider(ID_SAMPLE,
+                     names_from = taxon_name,
+                     values_from = taxon_count,
+                     names_sort = TRUE)
+
+# Store subsets ----
+CMPD <-
+  CMPD_metadata_3 %>%
+  dplyr::mutate(
+    clean = CMPD_clean %>%
+      dplyr::select(-c(ID_SAMPLE)),
+    intermediate = CMPD_intermediate %>%
+      dplyr::select(-c(ID_SAMPLE)),
+    amalgamated = CMPD_amalgamated %>%
+      dplyr::select(-c(ID_SAMPLE))
+  ) %>%
+  dplyr::mutate(
+    basin_size = basin_size %>%
+      stringr::str_replace_all("unknown", "not known"),
+    entity_type = entity_type %>%
+      stringr::str_replace_all("unknown", "not known") %>%
+      stringr::str_to_lower(),
+    site_type = site_type %>%
+      stringr::str_replace_all("estuarine", "coastal, estuarine") %>%
+      stringr::str_replace_all("terrestrial, soil", "soil") %>%
+      stringr::str_replace_all("unknown", "not known")
+  ) %>%
+  dplyr::relocate(ID_SAMPLE, .before = clean)
+
+usethis::use_data(CMPD, overwrite = TRUE, compress = "xz")
+
+## Inspect enumerates ----
+### basin_size -----
+CMPD$basin_size %>%
+  unique() %>% sort()
+
+### site_type ----
+CMPD$site_type %>%
+  unique() %>% sort()
+
+### entity_type ----
+CMPD$entity_type %>%
+  unique() %>% sort()
+
+# Export Excel workbook ----
+wb <- openxlsx::createWorkbook()
+openxlsx::addWorksheet(wb, "metadata")
+openxlsx::writeData(wb, "metadata",
+                    CMPD %>%
+                      dplyr::select(site_name:ID_SAMPLE))
+openxlsx::addWorksheet(wb, "clean")
+openxlsx::writeData(wb, "clean",
+                    CMPD %>%
+                      dplyr::select(ID_SAMPLE, clean) %>%
+                      tidyr::unnest(clean))
+openxlsx::addWorksheet(wb, "intermediate")
+openxlsx::writeData(wb, "intermediate",
+                    CMPD %>%
+                      dplyr::select(ID_SAMPLE, intermediate) %>%
+                      tidyr::unnest(intermediate))
+openxlsx::addWorksheet(wb, "amalgamated")
+openxlsx::writeData(wb, "amalgamated",
+                    CMPD %>%
+                      dplyr::select(ID_SAMPLE, amalgamated) %>%
+                      tidyr::unnest(amalgamated))
+openxlsx::saveWorkbook(wb,
+                       paste0("data-raw/GLOBAL/CMPD_",
+                              Sys.Date(),
+                              ".xlsx"))
