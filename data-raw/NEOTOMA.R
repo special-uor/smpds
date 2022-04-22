8b0b268eb25fd2f42ea07dff2b11204bc2ff9dc9
Roberto Villegas-Diaz <bthillo@hotmail.com>
2022-04-22 14:16:24 +0100

    Add code to create a revised version of the NEOTOMA subset

--- data-raw/NEOTOMA.R
+++ data-raw/NEOTOMA.R
@@ -1,4 +1,5 @@
 ## code to prepare `NEOTOMA` dataset goes here
+# Original ----
 neotoma_metadata <- readr::read_csv("inst/extdata/neotoma_metadata.csv")
 neotoma_count <- readr::read_csv("inst/extdata/neotoma_count.csv")
 neotoma_taxa <- readr::read_csv("inst/extdata/neotoma_taxa.csv")
@@ -123,3 +124,233 @@ neotoma_taxa <- readr::read_csv("~/Downloads/SMPDSv2/NEOTOMA_taxa_2021-08-24_SPH
   dplyr::filter(!is.na(taxon_name))
 neotoma_taxa %>%
   readr::write_excel_csv("inst/extdata/neotoma_taxa.csv", na = "")
+
+# SPH revisions ----
+"The entities and their counts were manually inspected by SPH"
+
+`%>%` <- magrittr::`%>%`
+## Load data ----
+### Metadata ----
+NEOTOMA_metadata <-
+  "data-raw/GLOBAL/Neotoma_extras_SPH.xlsx" %>%
+  readxl::read_excel(sheet = 1) %>%
+  dplyr::rename(doi = DOI) %>%
+  dplyr::mutate(ID_SAMPLE = seq_along(entity_name), .after = doi)
+
+### Pollen counts ----
+NEOTOMA_counts <-
+  "data-raw/GLOBAL/Neotoma_extras_SPH.xlsx" %>%
+  readxl::read_excel(sheet = 2) %>%
+  dplyr::left_join(NEOTOMA_metadata %>%
+                     dplyr::select(entity_name, ID_SAMPLE),
+                   by = "entity_name") %>%
+  dplyr::relocate(ID_SAMPLE, .before = 1)
+NEOTOMA_counts_2 <- NEOTOMA_counts %>%
+  dplyr::select(ID_SAMPLE) %>%
+  dplyr::bind_cols(
+    NEOTOMA_counts %>% # Convert columns with counts to numeric type
+      dplyr::select(-c(ID_SAMPLE:entity_name)) %>%
+      purrr::map_dfc(~.x %>% as.numeric)
+  ) %>%
+  magrittr::set_names(
+    colnames(.) %>%
+      stringr::str_replace_all("\\.\\.\\.", "#")
+  ) %>%
+  tidyr::pivot_longer(-ID_SAMPLE,
+                      names_to = "taxon_name",
+                      values_to = "taxon_count") %>%
+  dplyr::mutate(taxon_name = taxon_name %>%
+                  stringr::str_remove_all("\\#[0-9]+$") %>%
+                  stringr::str_squish()) %>%
+  dplyr::group_by(ID_SAMPLE, taxon_name) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::distinct() %>%
+  dplyr::ungroup()
+
+### Amalgamations ----
+NEOTOMA_taxa_amalgamation <-
+  "data-raw/GLOBAL/Neotoma_extras_SPH.xlsx" %>%
+  readxl::read_excel(sheet = 3) %>%
+  magrittr::set_names(c(
+   "taxon_name", "clean", "intermediate", "amalgamated"
+  )) %>%
+  dplyr::distinct() %>%
+  dplyr::mutate(taxon_name = taxon_name %>% stringr::str_squish(),
+                clean = clean %>% stringr::str_squish(),
+                intermediate = intermediate %>% stringr::str_squish(),
+                amalgamated = amalgamated %>% stringr::str_squish())
+
+### Combine counts and amalgamation ----
+NEOTOMA_taxa_counts_amalgamation <-
+  NEOTOMA_counts_2 %>%
+  dplyr::left_join(NEOTOMA_taxa_amalgamation,
+                   by = c("taxon_name")) %>%
+  dplyr::relocate(taxon_count, .after = amalgamated) %>%
+  dplyr::relocate(ID_SAMPLE, .before = 1) %>%
+  dplyr::select(-taxon_name)
+
+NEOTOMA_taxa_counts_amalgamation %>%
+  dplyr::filter(is.na(clean) | is.na(intermediate) | is.na(amalgamated)) %>%
+  dplyr::distinct(clean, intermediate, amalgamated)
+
+## Find DOIs ----
+NEOTOMA_metadata_pubs <-
+  NEOTOMA_metadata %>%
+  dplyr::distinct(publication, doi) %>%
+  dplyr::arrange(publication) %>%
+  dplyr::mutate(DOI = publication %>%
+                  stringr::str_extract_all("\\[DOI\\s*(.*?)\\s*\\](;|$)") %>%
+                  purrr::map_chr(~.x %>%
+                                   stringr::str_remove_all("^\\[DOI:") %>%
+                                   stringr::str_remove_all("\\]\\s*;\\s*$") %>%
+                                   stringr::str_remove_all("\\]$") %>%
+                                   stringr::str_remove_all("doi:") %>%
+                                   stringr::str_squish() %>%
+                                   stringr::str_c(collapse = ";\n"))
+  ) %>%
+  dplyr::mutate(ID_PUB = seq_along(publication)) %>%
+  dplyr::mutate(updated_publication = NA, .before = publication) %>%
+  dplyr::mutate(updated_DOI = NA, .before = DOI)
+# NEOTOMA_metadata_pubs %>%
+#   readr::write_excel_csv("data-raw/GLOBAL/NEOTOMA_modern-references.csv")
+
+### Load cleaned publications list ----
+NEOTOMA_clean_publications <-
+  "data-raw/GLOBAL/NEOTOMA_modern-references_clean.csv" %>%
+  readr::read_csv() %>%
+  dplyr::select(-DOI)
+
+## Append clean publications ----
+NEOTOMA_metadata_2 <-
+  NEOTOMA_metadata %>%
+  dplyr::left_join(NEOTOMA_metadata_pubs %>%
+                     dplyr::select(-DOI, -doi, -dplyr::contains("updated")),
+                   by = "publication") %>%
+  dplyr::left_join(NEOTOMA_clean_publications,
+                   by = "ID_PUB") %>%
+  dplyr::select(-publication.x, -publication.y, -doi, -ID_PUB) %>%
+  dplyr::rename(doi = updated_DOI,
+                publication = updated_publication)
+
+## Extract PNV/BIOME ----
+NEOTOMA_metadata_3 <-
+  NEOTOMA_metadata_2 #%>%
+# dplyr::select(-dplyr::starts_with("ID_BIOME")) %>%
+# smpds::parallel_extract_biome(cpus = 12) %>%
+# # smpds::biome_name() %>%
+# dplyr::relocate(ID_BIOME, .after = doi) %>%
+# smpds::pb()
+
+NEOTOMA_metadata_3 %>%
+  smpds::plot_biome(xlim = range(.$longitude, na.rm = TRUE) * c(0.9, 1.1),
+                    ylim = range(.$latitude, na.rm = TRUE) * c(0.9, 1.1))
+
+## Create count tables ----
+### Clean ----
+NEOTOMA_clean <-
+  NEOTOMA_taxa_counts_amalgamation %>%
+  dplyr::select(-intermediate, -amalgamated) %>%
+  dplyr::rename(taxon_name = clean) %>%
+  dplyr::group_by(ID_SAMPLE, taxon_name) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct() %>%
+  tidyr::pivot_wider(ID_SAMPLE,
+                     names_from = taxon_name,
+                     values_from = taxon_count,
+                     names_sort = TRUE) %>%
+  dplyr::arrange(ID_SAMPLE)
+
+### Intermediate ----
+NEOTOMA_intermediate <-
+  NEOTOMA_taxa_counts_amalgamation %>%
+  dplyr::select(-clean, -amalgamated) %>%
+  dplyr::rename(taxon_name = intermediate) %>%
+  dplyr::group_by(ID_SAMPLE, taxon_name) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct() %>%
+  tidyr::pivot_wider(ID_SAMPLE,
+                     names_from = taxon_name,
+                     values_from = taxon_count,
+                     names_sort = TRUE) %>%
+  dplyr::arrange(ID_SAMPLE)
+
+### Amalgamated ----
+NEOTOMA_amalgamated <-
+  NEOTOMA_taxa_counts_amalgamation %>%
+  dplyr::select(-clean, -intermediate) %>%
+  dplyr::rename(taxon_name = amalgamated) %>%
+  dplyr::group_by(ID_SAMPLE, taxon_name) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct() %>%
+  tidyr::pivot_wider(ID_SAMPLE,
+                     names_from = taxon_name,
+                     values_from = taxon_count,
+                     names_sort = TRUE) %>%
+  dplyr::arrange(ID_SAMPLE)
+
+# Store subsets ----
+NEOTOMA <-
+  NEOTOMA_metadata_3 %>%
+  dplyr::mutate(
+    clean = NEOTOMA_clean %>%
+      dplyr::select(-c(ID_SAMPLE)),
+    intermediate = NEOTOMA_intermediate %>%
+      dplyr::select(-c(ID_SAMPLE)),
+    amalgamated = NEOTOMA_amalgamated %>%
+      dplyr::select(-c(ID_SAMPLE))
+  ) %>%
+  dplyr::mutate(
+    basin_size = basin_size %>%
+      stringr::str_replace_all("unknown", "not known"),
+    entity_type = entity_type %>%
+      stringr::str_replace_all("ccore top", "core top") %>%
+      stringr::str_replace_all("unknown", "not known") %>%
+      stringr::str_to_lower(),
+    site_type = site_type %>%
+      stringr::str_replace_all("unknown", "not known")
+  ) %>%
+  dplyr::relocate(ID_SAMPLE, .before = clean)
+
+usethis::use_data(NEOTOMA, overwrite = TRUE, compress = "xz")
+
+## Inspect enumerates ----
+### basin_size -----
+NEOTOMA$basin_size %>%
+  unique() %>% sort()
+
+### site_type ----
+NEOTOMA$site_type %>%
+  unique() %>% sort()
+
+### entity_type ----
+NEOTOMA$entity_type %>%
+  unique() %>% sort()
+
+# Export Excel workbook ----
+wb <- openxlsx::createWorkbook()
+openxlsx::addWorksheet(wb, "metadata")
+openxlsx::writeData(wb, "metadata",
+                    NEOTOMA %>%
+                      dplyr::select(source:ID_SAMPLE))
+openxlsx::addWorksheet(wb, "clean")
+openxlsx::writeData(wb, "clean",
+                    NEOTOMA %>%
+                      dplyr::select(ID_SAMPLE, clean) %>%
+                      tidyr::unnest(clean))
+openxlsx::addWorksheet(wb, "intermediate")
+openxlsx::writeData(wb, "intermediate",
+                    NEOTOMA %>%
+                      dplyr::select(ID_SAMPLE, intermediate) %>%
+                      tidyr::unnest(intermediate))
+openxlsx::addWorksheet(wb, "amalgamated")
+openxlsx::writeData(wb, "amalgamated",
+                    NEOTOMA %>%
+                      dplyr::select(ID_SAMPLE, amalgamated) %>%
+                      tidyr::unnest(amalgamated))
+openxlsx::saveWorkbook(wb,
+                       paste0("data-raw/GLOBAL/NEOTOMA_",
+                              Sys.Date(),
+                              ".xlsx"))
