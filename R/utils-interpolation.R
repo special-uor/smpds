3c79ef4d5b235aa60a75f20969267cb09dc5f1d7
Roberto Villegas-Diaz <bthillo@hotmail.com>
2022-04-28 20:54:11 +0100

    Update the function create_sq_grid to include interpolations for elevation

--- R/utils-interpolation.R
+++ R/utils-interpolation.R
@@ -1,8 +1,14 @@
 #' @keywords internal
-create_sq_grid <- function(.data, resolution = 0.5) {
+create_sq_grid <- function(.data,
+                           resolution = 0.5,
+                           get_elevation =
+                             function(...) {
+                               return(tibble::tibble(elevation = 0))
+                               },
+                           cpus = 1,
+                           land_borders = NULL) {
   crs_raster_format <-
     "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0 +no_defs"
-  # "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0 +units=m +no_defs"
   .data_interp <- .data %>%
     dplyr::mutate(geometry = NA, .after = longitude) %>%
     sf::st_as_sf(
@@ -20,9 +26,30 @@ create_sq_grid <- function(.data, resolution = 0.5) {
     sf::st_as_sf() %>%
     cbind(., sf::st_coordinates(.)) %>%
     sf::st_drop_geometry() %>%
-    dplyr:::mutate(Z = 0)
+    dplyr:::mutate(Z = 0) %>%
+    magrittr::set_names(c("x", "y", "Z"))
 
+  # If land_borders are provided, apply as a mask to the grid
+  if (!missing(land_borders)) {
+    alt_grd_template_sf <- alt_grd_template_sf %>%
+      raster::rasterFromXYZ(
+        crs = crs_raster_format
+      ) %>%
+      raster::mask(mask = land_borders) %>%
+      raster::rasterToPoints()
+  }
+
+  # Extract values for elevation
+  .data2 <- alt_grd_template_sf %>%
+    tibble::as_tibble() %>%
+    dplyr::rename(longitude = x,
+                  latitude = y) %>%
+    get_elevation(cpus = cpus)
+
+  # Return new raster
   alt_grd_template_sf %>%
+    tibble::as_tibble() %>%
+    dplyr::mutate(Z = .data2$elevation) %>%
     raster::rasterFromXYZ(
       crs = crs_raster_format
     )
