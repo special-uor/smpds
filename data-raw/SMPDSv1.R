1c7540c0e124983253fa69736f97d87fae852044
Roberto Villegas-Diaz <bthillo@hotmail.com>
2022-05-11 15:44:34 +0100

    Update the SMPDSv1 to include changes made by SPH

--- data-raw/SMPDSv1.R
+++ data-raw/SMPDSv1.R
@@ -128,3 +128,346 @@ SMPDSv1_wide <- SMPDSv1_long %>%
 #   dplyr::select(dplyr::contains("..."))
 # idx <- rowSums(is.na(duplicated_taxa[, 4:6])) != 3
 # duplicated_taxa[idx,]
+
+
+# Export Excel workbook ----
+wb <- openxlsx::createWorkbook()
+openxlsx::addWorksheet(wb, "metadata")
+openxlsx::writeData(wb, "metadata",
+                    SMPDSv1 %>%
+                      dplyr::select(ID_SMPDSv1:DOI))
+openxlsx::addWorksheet(wb, "clean")
+openxlsx::writeData(wb, "clean",
+                    SMPDSv1 %>%
+                      dplyr::select(-c(source:DOI)) #%>%
+                    # tidyr::unnest(clean)
+)
+# openxlsx::addWorksheet(wb, "intermediate")
+# openxlsx::writeData(wb, "intermediate",
+#                     EMPDv2 %>%
+#                       dplyr::select(ID_EMPDv2, intermediate) %>%
+#                       tidyr::unnest(intermediate))
+# openxlsx::addWorksheet(wb, "amalgamated")
+# openxlsx::writeData(wb, "amalgamated",
+#                     EMPDv2 %>%
+#                       dplyr::select(ID_EMPDv2, amalgamated) %>%
+#                       tidyr::unnest(amalgamated))
+openxlsx::addWorksheet(wb, "taxon_list")
+openxlsx::writeData(wb, "taxon_list",
+                    "inst/extdata/all_taxa.csv" %>%
+                      readr::read_csv())
+openxlsx::saveWorkbook(wb,
+                       paste0("data-raw/SMPDSv1_",
+                              Sys.Date(),
+                              ".xlsx"))
+
+# SPH revisions ----
+"The entities and their counts were manually inspected by SPH"
+
+`%>%` <- magrittr::`%>%`
+## Load data ----
+SMPDSv1_all <-
+  "data-raw/GLOBAL/A_SMPDSv1_Iberia and EMBSECBIO cleanup done.xlsx" %>%
+  readxl::read_excel(sheet = 1) %>%
+  dplyr::rename(doi = DOI) %>%
+  dplyr::mutate(ID_SAMPLE = seq_along(entity_name), .after = doi) %>%
+  dplyr::select(-Publication)
+
+### Metadata ----
+SMPDSv1_metadata <-
+  SMPDSv1_all %>%
+  dplyr::select(source:ID_SAMPLE)
+
+### Polen counts ----
+SMPDSv1_counts <-
+  SMPDSv1_all %>%
+  dplyr::select(ID_SAMPLE) %>%
+  dplyr::bind_cols(
+    SMPDSv1_all %>% # Convert columns with counts to numeric type
+      dplyr::select(-c(source:ID_SAMPLE)) %>%
+      purrr::map_dfc(~.x %>% as.numeric)
+  ) %>%
+  magrittr::set_names(
+    colnames(.) %>%
+      stringr::str_replace_all("\\.\\.\\.", "#")
+  ) %>%
+  tidyr::pivot_longer(-ID_SAMPLE,
+                      names_to = "taxon_name",
+                      values_to = "taxon_count") %>%
+  dplyr::mutate(taxon_name = taxon_name %>%
+                  stringr::str_remove_all("\\#[0-9]+$") %>%
+                  stringr::str_squish()) %>%
+  dplyr::group_by(ID_SAMPLE, taxon_name) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::distinct() %>%
+  dplyr::ungroup()
+
+### Amalgamations ----
+SMPDSv1_taxa_amalgamation <-
+  "data-raw/GLOBAL/A_SMPDSv1_Iberia and EMBSECBIO cleanup done.xlsx" %>%
+  readxl::read_excel(sheet = 2) %>%
+  magrittr::set_names(c(
+    "clean", "intermediate", "amalgamated"
+  )) %>%
+  dplyr::distinct() %>%
+  purrr::map_df(stringr::str_squish)
+
+### Combine counts and amalgamation ----
+SMPDSv1_taxa_counts_amalgamation <-
+  SMPDSv1_counts %>%
+  dplyr::left_join(SMPDSv1_taxa_amalgamation,
+                   by = c("taxon_name" = "clean")) %>%
+  dplyr::relocate(taxon_count, .after = amalgamated) %>%
+  dplyr::relocate(ID_SAMPLE, .before = 1) %>%
+  dplyr::rename(clean = taxon_name)
+  # dplyr::select(-taxon_name)
+
+SMPDSv1_taxa_counts_amalgamation %>%
+  dplyr::filter(is.na(clean) | is.na(intermediate) | is.na(amalgamated)) %>%
+  dplyr::distinct(clean, intermediate, amalgamated)
+
+## Find DOIs ----
+SMPDSv1_metadata_pubs <-
+  SMPDSv1_metadata %>%
+  dplyr::distinct(publication, doi) %>%
+  dplyr::arrange(publication) %>%
+  dplyr::mutate(DOI = publication %>%
+                  stringr::str_extract_all("\\[DOI\\s*(.*?)\\s*\\](;|$)") %>%
+                  purrr::map_chr(~.x %>%
+                                   stringr::str_remove_all("^\\[DOI:") %>%
+                                   stringr::str_remove_all("\\]\\s*;\\s*$") %>%
+                                   stringr::str_remove_all("\\]$") %>%
+                                   stringr::str_remove_all("doi:") %>%
+                                   stringr::str_squish() %>%
+                                   stringr::str_c(collapse = ";\n"))
+  ) %>%
+  dplyr::mutate(ID_PUB = seq_along(publication)) %>%
+  dplyr::mutate(updated_publication = NA, .before = publication) %>%
+  dplyr::mutate(updated_DOI = NA, .before = DOI)
+# SMPDSv1_metadata_pubs %>%
+#   readr::write_excel_csv("data-raw/GLOBAL/SMPDSv1_modern-references.csv")
+
+### Load cleaned publications list ----
+SMPDSv1_clean_publications <-
+  "data-raw/GLOBAL/SMPDSv1_modern-references_clean.csv" %>%
+  readr::read_csv() %>%
+  dplyr::select(-DOI)
+
+## Append clean publications ----
+SMPDSv1_metadata_2 <-
+  SMPDSv1_metadata %>%
+  dplyr::left_join(SMPDSv1_metadata_pubs %>%
+                     dplyr::select(-DOI, -doi, -dplyr::contains("updated")),
+                   by = "publication") %>%
+  dplyr::left_join(SMPDSv1_clean_publications,
+                   by = "ID_PUB") %>%
+  dplyr::select(-publication.x, -publication.y, -doi, -ID_PUB) %>%
+  dplyr::rename(doi = updated_DOI,
+                publication = updated_publication)
+
+## Extract PNV/BIOME ----
+SMPDSv1_metadata_3 <-
+  SMPDSv1_metadata_2 %>%
+  dplyr::select(-dplyr::starts_with("ID_BIOME")) %>%
+  smpds::parallel_extract_biome(cpus = 10) %>%
+  # smpds::biome_name() %>%
+  dplyr::relocate(ID_BIOME, .after = doi) %>%
+  smpds::pb()
+
+SMPDSv1_metadata_3 %>%
+  smpds::plot_biome(xlim = range(.$longitude, na.rm = TRUE) * c(0.9, 1.1),
+                    ylim = range(.$latitude, na.rm = TRUE) * c(0.9, 1.1))
+
+## Create count tables ----
+### Clean ----
+SMPDSv1_clean <-
+  SMPDSv1_taxa_counts_amalgamation %>%
+  dplyr::select(-intermediate, -amalgamated) %>%
+  dplyr::rename(taxon_name = clean) %>%
+  dplyr::group_by(ID_SAMPLE, taxon_name) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct() %>%
+  tidyr::pivot_wider(ID_SAMPLE,
+                     names_from = taxon_name,
+                     values_from = taxon_count,
+                     names_sort = TRUE) %>%
+  dplyr::arrange(ID_SAMPLE)
+
+### Intermediate ----
+SMPDSv1_intermediate <-
+  SMPDSv1_taxa_counts_amalgamation %>%
+  dplyr::select(-clean, -amalgamated) %>%
+  dplyr::rename(taxon_name = intermediate) %>%
+  dplyr::group_by(ID_SAMPLE, taxon_name) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct() %>%
+  tidyr::pivot_wider(ID_SAMPLE,
+                     names_from = taxon_name,
+                     values_from = taxon_count,
+                     names_sort = TRUE) %>%
+  dplyr::arrange(ID_SAMPLE)
+
+### Amalgamated ----
+SMPDSv1_amalgamated <-
+  SMPDSv1_taxa_counts_amalgamation %>%
+  dplyr::select(-clean, -intermediate) %>%
+  dplyr::rename(taxon_name = amalgamated) %>%
+  dplyr::group_by(ID_SAMPLE, taxon_name) %>%
+  dplyr::mutate(taxon_count = sum(taxon_count, na.rm = TRUE)) %>%
+  dplyr::ungroup() %>%
+  dplyr::distinct() %>%
+  tidyr::pivot_wider(ID_SAMPLE,
+                     names_from = taxon_name,
+                     values_from = taxon_count,
+                     names_sort = TRUE) %>%
+  dplyr::arrange(ID_SAMPLE)
+
+# Extract missing elevations ----
+# SMPDSv1_metadata_4 <-
+#   SMPDSv1_metadata_3 %>%
+#   dplyr::rename(elevation_original = elevation) %>%
+#   smpds:::get_elevation(cpus = 12)
+#
+# SMPDSv1_metadata_4 %>%
+#   dplyr::select(ID_SAMPLE, entity_name, latitude, longitude, elevation_new = elevation, elevation_original) %>%
+#   dplyr::mutate(diff =
+#                   abs(elevation_new - elevation_original) / elevation_original) %>%
+#   # readr::write_csv("data-raw/GLOBAL/SMPDSv1_elevations_only.csv", na = "")
+#   dplyr::filter(diff >= 0.5)
+# dplyr::mutate(within_90p =
+#                 dplyr::between(elevation,
+#                                min(c(0.9, 1.1) * elevation_original),
+#                                max(c(0.9, 1.1) * elevation_original)),
+#               within_95p =
+#                 dplyr::between(elevation,
+#                                min(c(0.95, 1.05) * elevation_original),
+#                                max(c(0.95, 1.05) * elevation_original)),
+#               within_975p =
+#                 dplyr::between(elevation,
+#                                min(c(0.975, 1.025) * elevation_original),
+#                                max(c(0.975, 1.025) * elevation_original))
+# ) %>%
+#   dplyr::filter(!within_975p)
+
+# Store subsets ----
+SMPDSv1 <-
+  SMPDSv1_metadata_3 %>%
+  dplyr::mutate(
+    clean = SMPDSv1_clean %>%
+      dplyr::select(-c(ID_SAMPLE)),
+    intermediate = SMPDSv1_intermediate %>%
+      dplyr::select(-c(ID_SAMPLE)),
+    amalgamated = SMPDSv1_amalgamated %>%
+      dplyr::select(-c(ID_SAMPLE))
+  ) %>%
+  dplyr::mutate(
+    basin_size_old = basin_size,
+    basin_size_num = basin_size %>%
+      as.numeric() %>%
+      round(digits = 6) %>%
+      as.character(),
+    basin_size = dplyr::coalesce(
+      basin_size_num,
+      basin_size
+    ),
+    basin_size = basin_size %>%
+      stringr::str_to_lower() %>%
+      stringr::str_squish() %>%
+      stringr::str_replace_all("unknown|Unknown", "not known"),
+    entity_type = entity_type %>%
+      stringr::str_to_lower() %>%
+      stringr::str_squish() %>%
+      stringr::str_replace_all("unknown|Unknown", "not known") %>%
+      stringr::str_to_lower(),
+    site_type = site_type %>%
+      stringr::str_to_lower() %>%
+      stringr::str_squish() %>%
+      stringr::str_replace_all("estuarine", "coastal, estuarine") %>%
+      stringr::str_replace_all("drained/dry lake", "lacustrine, drained lake") %>%
+      stringr::str_replace_all("terrestrial, other sediments", "terrestrial") %>%
+      stringr::str_replace_all("terrestrial, soil", "soil") %>%
+      stringr::str_replace_all("unknown", "not known")
+  ) %>%
+  dplyr::relocate(ID_SAMPLE, .before = clean) %>%
+  # dplyr::relocate(basin_size_old, .after = basin_size) %>%
+  dplyr::select(-basin_size_num, -basin_size_old)
+
+usethis::use_data(SMPDSv1, overwrite = TRUE, compress = "xz")
+
+# Load climate reconstructions ----
+climate_reconstructions <-
+  "data-raw/reconstructions/SMPDSv1_climate_reconstructions_2022-04-30.csv" %>%
+  readr::read_csv()
+
+climate_reconstructions_with_counts <- smpds::SMPDSv1 %>%
+  dplyr::bind_cols(
+    climate_reconstructions %>%
+      dplyr::select(sn = site_name,
+                    en = entity_name,
+                    new_elevation = elevation,
+                    mi:mtwa)
+  ) %>%
+  dplyr::relocate(mi:mtwa, .before = clean) %>%
+  dplyr::mutate(elevation = dplyr::coalesce(elevation, new_elevation))
+climate_reconstructions_with_counts %>%
+  dplyr::filter(site_name != sn | entity_name != en)
+waldo::compare(smpds::SMPDSv1,
+               climate_reconstructions_with_counts %>%
+                 dplyr::select(-c(mi:mtwa))
+)
+SMPDSv1 <- climate_reconstructions_with_counts %>%
+  dplyr::select(-sn, -en, -new_elevation)
+usethis::use_data(SMPDSv1, overwrite = TRUE, compress = "xz")
+
+climate_reconstructions %>%
+  smpds::plot_climate_countour(
+    var = "mat",
+    xlim = range(.$longitude, na.rm = TRUE),
+    ylim = range(.$latitude, na.rm = TRUE)
+  )
+
+# Inspect enumerates ----
+### basin_size -----
+SMPDSv1$basin_size %>%
+  # stringr::str_replace_all("not applicable", "-888888") %>%
+  # stringr::str_replace_all("not known", "-999999") %>%
+  # as.numeric() %>%
+  # round(digits = 6) %>%
+  unique() %>%
+  sort()
+
+### site_type ----
+SMPDSv1$site_type %>%
+  unique() %>% sort()
+
+### entity_type ----
+SMPDSv1$entity_type %>%
+  unique() %>% sort()
+
+# Export Excel workbook ----
+wb <- openxlsx::createWorkbook()
+openxlsx::addWorksheet(wb, "metadata")
+openxlsx::writeData(wb, "metadata",
+                    SMPDSv1 %>%
+                      dplyr::select(source:ID_SAMPLE))
+openxlsx::addWorksheet(wb, "clean")
+openxlsx::writeData(wb, "clean",
+                    SMPDSv1 %>%
+                      dplyr::select(ID_SAMPLE, clean) %>%
+                      tidyr::unnest(clean))
+openxlsx::addWorksheet(wb, "intermediate")
+openxlsx::writeData(wb, "intermediate",
+                    SMPDSv1 %>%
+                      dplyr::select(ID_SAMPLE, intermediate) %>%
+                      tidyr::unnest(intermediate))
+openxlsx::addWorksheet(wb, "amalgamated")
+openxlsx::writeData(wb, "amalgamated",
+                    SMPDSv1 %>%
+                      dplyr::select(ID_SAMPLE, amalgamated) %>%
+                      tidyr::unnest(amalgamated))
+openxlsx::saveWorkbook(wb,
+                       paste0("data-raw/GLOBAL/SMPDSv1_",
+                              Sys.Date(),
+                              ".xlsx"))
